a24ae4b0d18252f5feef2f0ce959b03c43c7891c
Vignan Kurme <37151166+vignan37@users.noreply.github.com>
2023-01-20 21:48:38 +0530

    Create timesTwo.js

---
+++ Writing a Unit Test: Programming Assignment/timesTwo.js
@@ -0,0 +1,8 @@
+// Task 1: Code the timesTwo function declaration
+
+// Task 2: Export the timesTwo function as a module
+function timesTwo(number) {
+    return 2 * number;
+}
+
+module.exports = timesTwo;
